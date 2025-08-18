03eff767ef18a93ade9f7493c9dd7d4eeb4d0e81
Aanchal Kumari <135380408+Aanchal7915@users.noreply.github.com>
2025-08-18 19:26:12 +0000

    added commin navbar and footer to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Service from "./pages/Service";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-    <Home/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/service" element={<Service/>}/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
